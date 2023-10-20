6d0c108fc07a0e4ea972e1a8f228d6323999eff0
kirill <143942709+noeuseemo@users.noreply.github.com>
2023-10-20 20:00:10 +0700

    first commit

--- z1.py
+++ z1.py
@@ -1,7 +1,7 @@
-def tpl_sort(a):
-    for z in a:
-        if not isinstance(z, int):
+def tpl_sort(abv):
+    for element in abv:
+        if not isinstance(element, int):
             return ()
-    return tuple(sorted(a))
-print (tpl_sort((4, 6, 3, 1, 8)))
-print (tpl_sort((9, 6, 4, 2, 10)))
+        return tuple(sorted(abv))
+print(tpl_sort((4, 2, 1.9, 5, 8)))
+print(tpl_sort((2, 3, 2.6, 7, 9)))
